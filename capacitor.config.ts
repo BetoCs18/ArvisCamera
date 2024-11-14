35c3ca7bf48ecbea0aafb5e8f00f3cebb12057e0
BetoCs18 <albertocs1807@gmail.com>
2024-11-14 13:57:23 -0600

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,17 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.dticonsultores.arvistrainer',
   appName: 'ArvisCamera',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    "FFmpegStream": {
+      "ios": {
+        "class": "FFmpegStreamPlugin"
+      },
+      "android": {
+        "class": "FFmpegStreamPlugin"
+      }
+    }
+  }
 };
 
 export default config;
